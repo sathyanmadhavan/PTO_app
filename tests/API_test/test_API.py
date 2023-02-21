bb0df767cfa84430fc89aaa8c43a436c2cbaf809
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-02-21 16:53:48 +0530

    Updated test files

--- tests/API_test/test_API.py
+++ tests/API_test/test_API.py
@@ -1,21 +1,41 @@
 import requests
+import json
+import jsonpath
 
-BASIC_URL = "https://practice-testing-ai-ml.qxf2.com/"
+BASIC_URL = "http://127.0.0.1:6464/"
 
-class test_app():
-    def test_get_status_code_equals_200():
-        url = BASIC_URL
-        response = requests.get(url)
-        assert response.status_code == 200, print("Got wrong status code, expected is: {}, actual is {}".format("200", response.status_code))
 
-    def test_check_content_type_headers():
-        url = BASIC_URL
-        response = requests.get(url)
-        assert response.headers["Content-Type"] == "text/html; charset=utf-8",print(
-        "Got wrong content type in headers, expected is: {}, actual is {}".format("application/json; charset=utf-8",response.headers["Content-Type"]))
-    
+def test_get_status_code_equals_200():
+    url = BASIC_URL
+    response = requests.get(url)
+    assert response.status_code == 200 
+    print(response)
 
-app=test_app()
-test_app.test_check_content_type_headers()
+def test_check_content_type_headers():
+    url = BASIC_URL
+    response = requests.get(url)
+    assert response.headers["Content-Type"] == "text/html; charset=utf-8"
+    print(response.headers)
 
-test_app.test_get_status_code_equals_200()
+def test_check_page_title():
+    url = BASIC_URL
+    response = requests.get(url)
+    if response.status_code != 204 and response.headers["Content-Type"].startswith("application/json"):
+        json_data = response.json()
+        assert "title" in json_data
+        assert json_data["title"] == "Practice testing AI/ML based applications"
+
+def test_check_page_body():
+    url = BASIC_URL
+    response = requests.get(url)
+    if response.status_code != 204 and response.headers["Content-Type"].startswith("application/json"):
+        json_data = response.json()
+        assert "body" in json_data
+        assert json_data["body"] == "We would love to hear from you!"
+
+
+
+test_get_status_code_equals_200()
+test_check_content_type_headers()
+test_check_page_title()
+test_check_page_body()
--- tests/test_UI.py
+++ tests/test_UI.py
@@ -1,26 +1,36 @@
 from selenium import webdriver
 import time
-driver=webdriver.Chrome(executable_path='tests/chromedriver.exe')
+driver=webdriver.Chrome()
 driver.get('https://practice-testing-ai-ml.qxf2.com/')
-element = driver.find_element("xpath",'//a[@href="/is-pto"]').click()
-message=driver.find_element("xpath","//input[@type='text']").send_keys("I am sick today")
-Guess=driver.find_element("xpath","//button[@type='submit']").click()
 time.sleep(5)
 
-# Create a Facade class
-class test_UI:
-    def __init__(self, driver):
-        self.driver = driver
-    
-    def home(self):
-        self.driver.find_element("xpath","//img[@alt='Practice testing AI/ML based applications']").click()
-        time.sleep(5)
-    
-    def about(self):
-        self.driver.find_element("xpath",'//a[@href="/about"]').click()
-        time.sleep(5)
+def __init__(self, driver):
+    self.driver = driver
 
-# Create an instance of the Facade class
-facade = test_UI(driver)
-facade.home()
-facade.about()
+def test_about():
+    driver.find_element("xpath",'//a[@href="/about"]').click()
+    time.sleep(5)
+
+def test_home():
+    driver.find_element("xpath","//img[@alt='Practice testing AI/ML based applications']").click()
+    time.sleep(3)
+def test_is_pto():
+    driver.find_element("xpath",'//a[@href="/is-pto"]').click()
+    time.sleep(5)
+
+def test_message():
+    driver.find_element("xpath","//input[@type='text']").send_keys("I am sick today")
+    time.sleep(5)
+
+def test_guess():
+    driver.find_element("xpath","//button[@type='submit']").click()
+    time.sleep(3)
+
+# Execute the tests
+facade=driver
+test_about()
+test_home()
+test_is_pto()
+test_message()
+test_guess()
+
