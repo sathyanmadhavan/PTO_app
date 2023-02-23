52a15b554c7d30734a62752189e62df38606a771
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-02-23 19:33:12 +0530

    Updated test files

--- tests/API_test/test_API.py
+++ tests/API_test/test_API.py
@@ -17,18 +17,7 @@ def test_check_content_type_headers():
     assert response.headers["Content-Type"] == "text/html; charset=utf-8"
     print(response.headers)
 
-def test_check_page_title():
-    url = BASIC_URL
-    response = requests.get(url)
-    if response.status_code != 204 and response.headers["Content-Type"].startswith("text/html"):
-        f = open('data.json')
-        json_data = json.load(f)
-        print(json_data)
-        assert "title" in json_data
-        if json_data["title"] == "Practice testing AI/ML based applications":
-            print("Passed")
-        else:
-            print("Fail")
+
 
 def test_response():
     url = BASIC_URL
@@ -42,5 +31,4 @@ def test_response():
 
 test_get_status_code_equals_200()
 test_check_content_type_headers()
-test_check_page_title()
 test_response()
