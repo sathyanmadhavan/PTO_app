f8d434c616eb7ffe6e159f0608cfcc52b7198d6d
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-02-23 18:17:57 +0530

    Updated files

--- tests/test_UI.py
+++ tests/test_UI.py
@@ -21,25 +21,26 @@ for option in options:
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 driver.get('https://practice-testing-ai-ml.qxf2.com/')
-element = driver.find_element("xpath",'//a[@href="/is-pto"]').click()
-message=driver.find_element("xpath","//input[@type='text']").send_keys("I am sick today")
-Guess=driver.find_element("xpath","//button[@type='submit']").click()
-time.sleep(5)
 
-# Create a Facade class
-class test_UI:
-    def __init__(self, driver):
-        self.driver = driver
+def __init__(self, driver):
+    self.driver = driver
     
-    def home(self):
-        self.driver.find_element("xpath","//img[@alt='Practice testing AI/ML based applications']").click()
-        time.sleep(5)
+def test_home(self):
+    self.driver.find_element("xpath","//img[@alt='Practice testing AI/ML based applications']").click()
+    time.sleep(5)
+
+def test_input(self):
+     self.driver.find_element("xpath",'//a[@href="/is-pto"]').click()
+     self.driver.find_element("xpath","//input[@type='text']").send_keys("I am sick today")
+     self.driver.find_element("xpath","//button[@type='submit']").click()
+     time.sleep(5)
     
-    def about(self):
-        self.driver.find_element("xpath",'//a[@href="/about"]').click()
-        time.sleep(5)
+def test_about(self):
+    self.driver.find_element("xpath",'//a[@href="/about"]').click()
+    time.sleep(5)
 
 # Create an instance of the Facade class
-facade = test_UI(driver)
-facade.home()
-facade.about()
+facade = driver
+test_home()
+test_input()
+test_about()
